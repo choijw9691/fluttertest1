806b191cabb8ec864780cc91534efeb994a2b2cf
jiwoungchoi <choijw969133@gmail.com>
2023-06-13 01:49:04 +0900

    j

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,20 @@ class MyApp extends StatelessWidget {
           SizedBox(height: 80,),
             Text(style: TextStyle(color: Colors.white.withOpacity(0.8),fontSize: 20),'ffffff'),
             Text(style: TextStyle(color: Colors.white.withOpacity(0.8),fontSize: 40,fontWeight: FontWeight.w600),'ffffff'),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(45),
+                    color: Colors.amber
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20,horizontal: 40),child: Text('Transfer',style: TextStyle(fontSize: 22),),
+                  ),
+                )
+              ],
+            )
           ],
         ),),backgroundColor: Color(0xFF181818),
       ),
